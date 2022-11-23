308da313a1ff83c617ec2f0bf30290e7383d39f9
Andrew Li <zeyu.li23@gmail.com>
2022-11-23 10:22:35 -0700

    fixed resume pdf

--- src/components/resume/resume.tsx
+++ src/components/resume/resume.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const ResumeDoc = () => {
